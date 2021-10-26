669f70672cbe8ab2bb3cdc0d1e51510fb0392140
Mike Kutsepov <spartan-mofack@ya.ru>
2021-10-26 07:02:22 -0700

    code changed

--- WoodruffTestFunction/hello_world.cs
+++ WoodruffTestFunction/hello_world.cs
@@ -27,7 +27,7 @@ namespace WoodruffTestFunction
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Greetings, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
